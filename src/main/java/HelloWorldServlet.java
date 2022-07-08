6e4ff292a9594ba21d33d5359a92d77c6cd917ba
“Cory <coryurton@gmail.com>
2022-07-08 10:38:57 -0500

    servlets exercises complete

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,14 +5,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
+
+    int count = 0;
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        out.println("<h1>Hello World!</h1>");
+
+        count+= 1;
+        out.println(count);
+
     }
 
 }
---
+++ src/main/java/PageCounterServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/count")
+public class PageCounterServlet extends HttpServlet {
+
+    int pageCount = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        PrintWriter out = resp.getWriter();
+
+        pageCount+= 1;
+        out.println("<h1> Page visits so far: " + pageCount + "</h1>");
+    }
+}
